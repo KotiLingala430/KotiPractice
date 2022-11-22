dd5a655d181eb156b1ffffe79e27b654dc1b6e09
koti <ACER@LAPTOP-VUF6S1B0>
2022-11-22 12:03:57 +0530

    22-11-2022 : Added Java Basics class

--- KotiPractice/src/com/practice/JavaBasics.java
+++ KotiPractice/src/com/practice/JavaBasics.java
@@ -3,7 +3,41 @@ package com.practice;
 public class JavaBasics {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		System.out.println("Arithmatic Operators");
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		System.out.print("................................");
+		
+		System.out.println("Relational Operators");
+		System.out.println(a>b);
+		System.out.println(a<b);
+		System.out.println(a<=b);
+		System.out.println(a>=b);
+		System.out.println(a==b);
+		System.out.println(a!=b);
+		System.out.println("..............................");
+		
+		
+		boolean x=true;
+		boolean y=false;
+		System.out.println("Logical Operators");
+		System.out.println(x && y);
+		System.out.println(x||y);
+		System.out.println("..............................");
+		
+		int c;
+		c=a+b;
+		System.out.println("Assignment Operators");
+		System.out.println(c);
+		c++;
+		System.out.println(c);
+		c--;
+		System.out.println(c);
 
 	}
 
