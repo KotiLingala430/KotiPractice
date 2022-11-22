b87737ad2634221074b6c677b1f4a06c68cc9a80
koti <koteswarao430@gmail.com>
2022-11-22 18:34:44 +0530

    22-11-2022:added eligibility criteria for election

---
+++ KotiPractice/src/com/practice/ConditionalStatements.java
@@ -0,0 +1,19 @@
+package com.practice;
+
+public class ConditionalStatements {
+
+	public static void main(String[] args) {
+		int i =18;
+		if(i>=18) {
+			System.out.println("He is eligible to Vote" + ":"+i);
+		
+		}
+		else{
+			
+			System.out.println("He is not Eligible to Vote"+ ":"+i);
+		}
+		System.out.println("Minium age criteria to Vote is 18");
+
+	}
+
+}
