3228f625be8452f34d15e94d107b6271dc451646
koti <koteswarao430@gmail.com>
2022-11-22 18:47:01 +0530

    commit

--- KotiPractice/src/com/practice/ConditionalStatements.java
+++ KotiPractice/src/com/practice/ConditionalStatements.java
@@ -4,15 +4,27 @@ public class ConditionalStatements {
 
 	public static void main(String[] args) {
 		int i =18;
-		if(i>=18) {
-			System.out.println("He is eligible to Vote" + ":"+i);
+//		if(i>=18) {
+//			System.out.println("He is eligible to Vote" + ":"+i);
+//		
+//		}
+//		else{
+//			
+//			System.out.println("He is not Eligible to Vote"+ ":"+i);
+//		}
+//		System.out.println("Minium age criteria to Vote is 18");
+		if(i==1) {
+			System.out.println("Monday");
+		}else if(i==2) {
+			System.out.println("Tuesday");
+		}else if(i==3) {
+			System.out.println("Wednesday");
+		}else if(i==4) {
+			System.out.println("Thursday");
+		}else if(i==5) {
+			System.out.println("Friday");
+		}
 		
-		}
-		else{
-			
-			System.out.println("He is not Eligible to Vote"+ ":"+i);
-		}
-		System.out.println("Minium age criteria to Vote is 18");
 
 	}
 
