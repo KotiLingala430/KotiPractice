50bc9111eea886601e857b9184be50774f9fcc64
koti <koteswarao430@gmail.com>
2022-12-10 12:12:43 +0530

    Reading of elements through different loops

--- KotiPractice/src/javaCollections/ArrayLists.java
+++ KotiPractice/src/javaCollections/ArrayLists.java
@@ -5,7 +5,7 @@ public class ArrayLists {
 	public static void main(String[] args) {
 
 		//Declaration
-		ArrayList<Comparable> al = new ArrayList<Comparable>();
+		ArrayList al = new ArrayList();
 		al.add(100);
 		al.add("Koti");
 		al.add(12.5);
@@ -24,8 +24,35 @@ public class ArrayLists {
 		System.out.println("Elements after inserting :"+al);
 		//Retrieve element
 		System.out.println(al.get(3));
+		System.out.println("---------------------");
+		
+		for (int i = 0; i <al.size(); i++) {
+			System.out.println(al.get(i));
+			
+		}
+		System.out.println("---------------------");
+		for (Object o : al) {
+			
+			System.out.println(o);
+		}
+		
+		System.out.println("---------------------");
+		Iterator it = al.iterator();
+		while(it.hasNext()) {
+			System.out.println(it.next());
+		}
+		System.out.println("---------------------");
+		
+		String st[] = {"Cycle", "Bike", "Car", "Lorry"};
+		for (String string : st) {
+			System.out.println(string);
+			
+		}
+		 ArrayList al1 = new ArrayList(Arrays.asList(st));
+		 System.out.println(al1);
+		}
 		
 
 	}
 
-}
+
